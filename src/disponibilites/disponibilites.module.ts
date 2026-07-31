d9449c79f75e0acdee5eaa01aeeab59d7f156e25
Wadebe <wadebe2016@gmail.com>
2026-08-01 00:41:46 +0100

    Route praticiens pour le frontend disponibilites

--- src/disponibilites/disponibilites.module.ts
+++ src/disponibilites/disponibilites.module.ts
@@ -2,10 +2,11 @@
 import { PrismaModule } from '../prisma/prisma.module';
 import { DisponibilitesService } from './disponibilites.service';
 import { DisponibilitesController } from './disponibilites.controller';
+import { PraticiensController } from './praticiens.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [DisponibilitesController],
+  controllers: [DisponibilitesController, PraticiensController],
   providers: [DisponibilitesService],
   // Exporte pour que le module rendez-vous puisse verifier les conflits
   exports: [DisponibilitesService],
---
+++ src/disponibilites/praticiens.controller.ts
@@ -0,0 +1,18 @@
+﻿import { Controller, Get, Req } from '@nestjs/common';
+import { Permissions } from '../auth/permissions.decorator';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Controller('praticiens')
+export class PraticiensController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  @Permissions('rdv.lire')
+  lister(@Req() req: any) {
+    return this.prisma.praticien.findMany({
+      where: { hopitalId: req.user.hopitalId, deletedAt: null },
+      select: { id: true, nom: true, prenom: true, specialite: true },
+      orderBy: { nom: 'asc' },
+    });
+  }
+}
